a1582a42d080fa969d6405542ce8d428d4849280
Mnde81 <minubis@gmail.com>
2024-09-19 11:16:50 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TestimonialsApp from './components/TestimonialsApp'
+
+function App() {
+ 
+
+  return (
+    <>
+   <TestimonialsApp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+function Button({btnClass, text, onClick}) {
+ 
+
+    return (
+      <>
+     <button className={`btn ${btnClass} btn-sm`} onClick={onClick}>{text}</button>
+      </>
+    )
+  }
+  
+  export default Button;
+  
